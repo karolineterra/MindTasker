09c4da2f3fd06f84af71b6aa51f0d768ab22bb3b
andressa <andressaleal@lett.digital>
2023-09-27 22:27:07 -0300

    adicionando componentes login

--- my-app/src/containers/Login.js
+++ my-app/src/containers/Login.js
@@ -1,8 +1,17 @@
 import React from "react";
+import BesideForm from "../components/BesideForm";
+import '../styles/Login.css';
 
 function Login() {
     return (
-        <div>Estou funcionando, sou a tela de login</div>
+        <div className="LoginBody">
+            <BesideForm/>
+            <form>
+                <h1>Login</h1>
+                <label for="email">E-mail</label>
+                <input name="email" type="text" placeholder="Enter your email"></input>
+            </form>
+        </div>
     );
 }
 
