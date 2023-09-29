a2d81a84270957fe0b1bb55dfbb97f778736ebe4
andressa <andressaleal@lett.digital>
2023-09-28 23:22:08 -0300

    adding-form-inputs

--- my-app/src/containers/CreateAccount.js
+++ my-app/src/containers/CreateAccount.js
@@ -1,8 +1,25 @@
 import React from "react";
+import BesideForm from "../components/BesideForm";
+import '../styles/Login.css';
 
 function CreateAccount() {
     return (
-        <div>Estou funcionando, sou a tela de login</div>
+        <div className="CreateAccountBody">
+            <BesideForm/>
+            <form>
+                <h1>Create Your Account</h1>
+                <label for="name">Name</label>
+                <input name="name" type="text" placeholder="Enter your name"></input>
+                <label for="email">E-mail</label>
+                <input name="email" type="email" placeholder="Enter your email"></input>
+                <label for="password">Passwordl</label>
+                <input name="password" type="password" placeholder="Enter your password"></input>
+                <label for="birth">Birth</label>
+                <input name="birth" type="date"></input>
+                <input type="submit">Confirm</input>
+                <a href="#">Already have an account? Click here!</a>
+            </form>
+        </div>
     );
 }
 
--- my-app/src/containers/Login.js
+++ my-app/src/containers/Login.js
@@ -9,7 +9,12 @@ function Login() {
             <form>
                 <h1>Login</h1>
                 <label for="email">E-mail</label>
-                <input name="email" type="text" placeholder="Enter your email"></input>
+                <input name="email" type="email" placeholder="Enter your email"></input>
+                <label for="password">Passwordl</label>
+                <input name="password" type="password" placeholder="Enter your password"></input>
+                <a href="#">I forgot my password</a>
+                <input type="submit">Log in</input>
+                <a href="#">Don't have an account? Click here!</a>
             </form>
         </div>
     );
