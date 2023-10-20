8fbfea4578888200f96fe9afe3bf5ace51904832
karolineterra <terra.karoline1@gmail.com>
2023-10-19 21:23:15 -0300

    formulario de edição de perfil

--- my-app/src/components/EditSettings.js
+++ my-app/src/components/EditSettings.js
@@ -1,38 +1,111 @@
 import React, { useState } from "react";
-import userImage from "../assets/userImage.png"
+import userImage from "../assets/userImage.png";
 
-import '../styles/Settings.css';
+import "../styles/EditSettings.css";
 
 function EditSettings() {
-    const [image, setImage] = useState(userImage);
-    const fileInputRef = React.createRef();
+  //variaveis e funções da edição de imagem
+  const [image, setImage] = useState(userImage);
+  const fileInputRef = React.createRef();
 
-    const handleImageUpload = (event) => {
-        setImage(URL.createObjectURL(event.target.files[0]));
-    };
+  const handleImageUpload = (event) => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
 
-    const triggerFileInput = () => {
-        fileInputRef.current.click();
-    };
+  const triggerFileInput = () => {
+    fileInputRef.current.click();
+  };
 
-    return (
-        <div className="profileInformation">
-            <h1>Profile</h1>
-            <div className="informationPanel">
+  //variáveis e funções do formulário
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    age: "",
+    gender: "",
+  });
 
-                <div style={{ 
-                    backgroundImage: `url(${image})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover'}} class="profilePictureContainer"></div>
-            </div>
-            <div className="buttonsContainer">
-                <button className="editPictureSettings" onClick={triggerFileInput}>Edit Picture</button>
-                <input type="file" accept="image/*" onChange={handleImageUpload} style={{display: 'none'}} ref={fileInputRef} />
-                <a className="editProfileSettings" href="/settings">Edit Profile</a>
-            </div>
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <div className="profileInformationEditSettings">
+      <h1>Profile</h1>
+      <div className="profileInformationEditSettingsRow">
+        <div className="profileInformationEditLeftContainer">
+          <div
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            class="profilePictureContainer"
+          ></div>
+          <div className="buttonsContainer">
+            <button className="editPictureSettings" onClick={triggerFileInput}>
+              Edit Picture
+            </button>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              style={{ display: "none" }}
+              ref={fileInputRef}
+            />
+          </div>
         </div>
-    );
+        <div className="profileInformationEditRightContainer">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Idade:
+              <input
+                type="number"
+                name="age"
+                value={form.age}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Gênero:
+              <select name="gender" value={form.gender} onChange={handleChange}>
+                <option value="">Selecione...</option>
+                <option value="male">Masculino</option>
+                <option value="female">Feminino</option>
+                <option value="other">Outro</option>
+              </select>
+            </label>
+            <button className="editProfileSettings"type="submit">Save</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditSettings;
-
