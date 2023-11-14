14b13d023ca7135c7028116af206d38262c2b19e
andressa <andressaleal@lett.digital>
2023-11-13 23:39:09 -0300

    fixing notes

--- my-app/src/components/Note.js
+++ my-app/src/components/Note.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import editIcon from '../assets/editIcon.png';
+import Save from '../assets/Save.png';
 import "../styles/Note.css";
 
 function Note() {
@@ -16,13 +17,13 @@ function Note() {
   return (
     <div className="noteContainer">
         <div className="noteHeader">
-            <h3 className='noteTitle'>
+            <h3 className={`noteTitle ${isEdit ? 'editMode' : ''}`}>
                 {isEdit ? (<textarea value={note.title} onChange={ (event) => {
                 const newTitle = event.target.value;
                 const updatedNote = { ...note, title: newTitle };
                 setNote(updatedNote);
             }}/>) : (note.title )}</h3>
-            <button onClick={toggleEdit}>{isEdit ? 'Save' : <img src={editIcon} alt="Edit Icon" />}</button>
+            <button onClick={toggleEdit}>{isEdit ? <img src={Save} alt="Save Icon" /> : <img src={editIcon} alt="Edit Icon" />}</button>
         </div>
         <div className={`noteText ${isEdit ? 'editMode' : ''}`}>
             {isEdit ? (<textarea value={note.content} onChange={ (event) => {
